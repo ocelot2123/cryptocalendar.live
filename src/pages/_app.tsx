5e734a85c3ec17f13b58c32958abba0350b53070
ocelot2123 <edwardfu850808@gmail.com>
2022-07-14 21:30:49 +0800

    add head to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react';
 
-import theme from "../theme";
-import { AppProps } from "next/app";
+import theme from '../theme';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Crypto Calendar</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+        <meta name="description" content={'Crypto Calendar'} />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
