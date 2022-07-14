48036072be63509262d22ae456ae3130d3a4c74f
ocelot2123 <edwardfu850808@gmail.com>
2022-07-14 21:31:17 +0800

    remove maximum scale

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,10 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Crypto Calendar</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content={'Crypto Calendar'} />
       </Head>
       <ChakraProvider theme={theme}>
